9d47ba183f364218e78302e7a7584c769ff041d7
Apokolipzz <Junioraniekan1@gmail.com>
2025-03-14 22:27:07 +0000

    Updated

--- rest.py
+++ rest.py
@@ -5,3 +5,4 @@ def rest():
     random_minutes = randint(1,3)
     print(f"Waiting for {random_minutes} minute{"s" if random_minutes > 1 else ""} before sending next question...\n")
     sleep(random_minutes * 60)
+    
